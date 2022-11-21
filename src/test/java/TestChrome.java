94ce165fe06ce452dc2223808c4a51e96e3ce24b
Marat <marat.grishin@gmail.com>
2022-11-21 20:55:52 +0200

    [master]: Project initial upload

---
+++ src/test/java/TestChrome.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class TestChrome {
+    ChromeDriver driver = new ChromeDriver();
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void chromeTest() {
+        driver.get("https://google.lv/?hl=en");
+
+        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
+        acceptButton.click();
+
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("acodemy");
+
+        searchField.sendKeys(Keys.ENTER);
+
+        Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
+    }
+}
+
